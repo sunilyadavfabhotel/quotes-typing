999d2dbddcc9d009dbb082282da31dbdc018f5bc
Sunil Kumar Yadav <123sunilkr@gmail.com>
2023-01-02 14:15:12 +0530

    Add: types file

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+// An enum with all the types of actions to use in our Quotes reducer
+enum QuotesActionKind {
+  SMILE = "smile",
+  ADVICE_1 = "ADVICE_1",
+  ADVICE_2 = "ADVICE_2",
+  ADVICE_3 = "ADVICE_3",
+  ADVICE_4 = "ADVICE_4",
+  ADVICE_5 = "ADVICE_5",
+  ALPH_MAN = "ALPH_MAN",
+  FREEDOM_IS_WORTH_FIGHTING = "FREEDOM_IS_WORTH_FIGHTING",
+  ALL = "ALL",
+}
+
+const OPTIONS = [
+  { type: QuotesActionKind.ALL, label: "All Quotes" },
+  { type: QuotesActionKind.ADVICE_1, label: "Advice 1" },
+  { type: QuotesActionKind.ADVICE_2, label: "Advice 2" },
+  { type: QuotesActionKind.ADVICE_3, label: "Advice 3" },
+  { type: QuotesActionKind.ADVICE_4, label: "Advice 4" },
+  { type: QuotesActionKind.ADVICE_5, label: "Advice 5" },
+  { type: QuotesActionKind.ALPH_MAN, label: "Alpha man qualities" },
+  {
+    type: QuotesActionKind.FREEDOM_IS_WORTH_FIGHTING,
+    label: "Freedom is worth fighting",
+  },
+];
+
+// An interface for our Qoutes action
+interface QuotesAction {
+  type: QuotesActionKind;
+}
+
+// An interface for our Qoutes state
+interface QuotesState {
+  [index: number]: string;
+}
+
+export type { QuotesAction, QuotesState };
+export { QuotesActionKind, OPTIONS };
